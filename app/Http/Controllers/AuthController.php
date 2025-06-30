95e5f9a63e22c5e8e0b8db4815040f6a17cdf024
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-06-30 21:24:54 +0800

    UPDATE  :Perbaiakn url saat setelah login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -80,7 +80,7 @@ class AuthController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect('/');
+        return redirect('/dashboard');
     }
 
 
