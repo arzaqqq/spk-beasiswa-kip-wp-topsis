a5a5f1eb920680aaa39e1d92f3bf493bd7d792a0
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-07-11 12:34:24 +0700

    UPDATE : Perbaikan logo pada aside bar

--- resources/views/perhitungan/wp.blade.php
+++ resources/views/perhitungan/wp.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.app')
 @section('title', 'Perhitungan WP (Weighted Product)')
 
+@push('styles')
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+@endpush
+
 @section('content')
 <div class="container mx-auto px-4 py-6">
     <h2 class="text-2xl font-bold mb-6">Perhitungan Metode WP</h2>
@@ -130,3 +136,18 @@
     </div>
 </div>
 @endsection
+
+
+@push('scripts')
+   <script src="{{ asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('plugins/jszip/jszip.min.js') }}"></script>
+<script src="{{ asset('plugins/pdfmake/pdfmake.min.js') }}"></script>
+<script src="{{ asset('plugins/pdfmake/vfs_fonts.js') }}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+<script src="{{ asset('plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+
+@endpush
